3e8bce6a5960e488d138fa1b07dc7d661ef72f35
koba <kobagogoberidze21@gmail.com>
2023-01-16 00:18:31 +0400

    changed container id to lowercase

--- properties_dialog.php
+++ properties_dialog.php
@@ -6,7 +6,7 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
 /** @var \Bitrix\Bizproc\Activity\PropertiesDialog $dialog */
 
 foreach ($dialog->getMap() as $fieldId => $field) : ?>
-    <tr id="field_<?= htmlspecialcharsbx($field['FieldName']) ?>_container">
+    <tr id="field_<?= strtolower(htmlspecialcharsbx($field['FieldName'])) ?>_container">
         <td align="right" width="40%"><?= htmlspecialcharsbx($field['Name']) ?>:</td>
         <td width="60%">
             <?php
