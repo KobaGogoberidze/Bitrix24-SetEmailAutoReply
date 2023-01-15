9dea4d6a184a08e1d56b9f13ad3868872978b3b9
koba <kobagogoberidze21@gmail.com>
2023-01-15 18:22:33 +0400

    Restructure

--- properties_dialog.php
+++ properties_dialog.php
@@ -1,21 +1,22 @@
 <?
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-?>
-<tr>
-    <td align="right" width="40%" valign="top"><span class="adm-required-field"><?= GetMessage("JC_SEAR_EMPLOYEES_FIELD_TITLE") ?>:</span></td>
-    <td width="60%">
-        <?= CBPDocument::ShowParameterField("user", 'Employees', $arCurrentValues['Employees'], array('rows' => 1, 'cols' => 50)) ?>
-    </td>
-</tr>
-<tr>
-    <td align="right" width="40%" valign="top"><span class="adm-required-field"><?= GetMessage("JC_SEAR_AUTO_REPLY_CONTENT_FIELD_TITLE") ?>:</span></td>
-    <td width="60%">
-        <?= CBPDocument::ShowParameterField("text", 'AutoReplyContent', $arCurrentValues['AutoReplyContent'], array('rows' => 10, 'cols' => 50)) ?>
-    </td>
-</tr>
-<tr>
-    <td align="right" width="40%" valign="top"><span class="adm-required-field"><?= GetMessage("JC_SEAR_SET_READ_STATUS_FIELD_TITLE") ?>:</span></td>
-    <td width="60%">
-        <?= CBPDocument::ShowParameterField("bool", 'SetReadStatus', $arCurrentValues['SetReadStatus']) ?>
-    </td>
-</tr>
+
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+/** @var \Bitrix\Bizproc\Activity\PropertiesDialog $dialog */
+
+foreach ($dialog->getMap() as $fieldId => $field) : ?>
+    <tr id="field_<?= htmlspecialcharsbx($field['FieldName']) ?>_container">
+        <td align="right" width="40%"><?= htmlspecialcharsbx($field['Name']) ?>:</td>
+        <td width="60%">
+            <?php
+            $fieldType = $dialog->getFieldTypeObject($field);
+
+            echo $fieldType->renderControl(array(
+                'Form' => $dialog->getFormName(),
+                'Field' => $field['FieldName']
+            ), $dialog->getCurrentValue($field['FieldName']), true, 0);
+            ?>
+        </td>
+    </tr>
+<?php endforeach; ?>
