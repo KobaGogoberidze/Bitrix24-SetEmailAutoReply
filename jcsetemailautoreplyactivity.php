aed70d855afe160c01467ab27d5d72ff8d35b8de
koba <kobagogoberidze21@gmail.com>
2023-01-15 13:26:59 +0400

    Implement properties dialog processor

--- jcsetemailautoreplyactivity.php
+++ jcsetemailautoreplyactivity.php
@@ -17,12 +17,14 @@ class CBPJCSetEmailAutoReplyActivity extends CBPActivity
             "Title" => "",
             "Employees" => "",
             "AutoReplyContent" => "",
-            "SetReadStatus" => false
+            "SetReadStatus" => false,
+            "Rules" => ""
         );
 
         $this->SetPropertiesTypes(array(
             "Employees" => array(
-                "Type" => FieldType::USER
+                "Type" => FieldType::USER,
+                "Multiple" => "Y"
             ),
             "AutoReplyContent" => array(
                 "Type" => FieldType::STRING
@@ -30,6 +32,10 @@ class CBPJCSetEmailAutoReplyActivity extends CBPActivity
             "SetReadStatus" => array(
                 "Type" => FieldType::BOOL
             ),
+            "Rules" => array(
+                "Type" => FieldType::INT,
+                "Multiple" => "Y"
+            )
         ));
     }
     /**
@@ -56,24 +62,26 @@ class CBPJCSetEmailAutoReplyActivity extends CBPActivity
             "SORT" => 10,
             "WHEN_MAIL_RECEIVED" => "Y",
             "WHEN_MANUALLY_RUN" => "N",
-            "ACTION_READ" => "Y"
+            "ACTION_READ" => "Y",
+            "ACTION_PHP" => str_replace("#AUTO_REPLY_CONTENT#", $this->AutoReplyContent, $this->GetAutoReplyProcessor())
         );
+
+        $arRules = array();
 
         foreach ($arEmployees as $employeeID) {
             if ($arMailBox = CMailBox::GetList(array(), array("USER_ID" => $employeeID))) {
-                CMailFilter::Add(
+                $arRules[] = CMailFilter::Add(
                     array_merge(
                         $arRuleFields,
-                        array(
-                            "MAILBOX_ID" => $arMailBox["ID"],
-                            "ACTION_PHP" => str_replace("#AUTO_REPLY_CONTENT#", $this->AutoReplyContent, $this->GetAutoReplyProcessor())
-                        )
+                        array("MAILBOX_ID" => $arMailBox["ID"])
                     )
                 );
             } else {
                 $this->WriteToTrackingService(str_replace("#EMPLOYEE", $employeeID, GetMessage("JC_WL2F_FILE_PATH_EMPTY")), 0, CBPTrackingType::Error);
             }
         }
+
+        $this->arRules = $arRules;
 
         return CBPActivityExecutionStatus::Closed;
     }
@@ -92,6 +100,32 @@ class CBPJCSetEmailAutoReplyActivity extends CBPActivity
      */
     public static function GetPropertiesDialog($documentType, $activityName, $arWorkflowTemplate, $arWorkflowParameters, $arWorkflowVariables, $arCurrentValues = null, $formName = "")
     {
+        $runtime = CBPRuntime::GetRuntime();
+
+        if (!is_array($arCurrentValues)) {
+            $arCurrentValues = array(
+                "Employees" => "",
+                "AutoReplyContent" => "",
+                "SetReadStatus" => false,
+            );
+
+            $arCurrentActivity = &CBPWorkflowTemplateLoader::FindActivityByName($arWorkflowTemplate, $activityName);
+
+            if (is_array($arCurrentActivity["Properties"])) {
+                $arCurrentValues["Employees"] = CBPHelper::UsersArrayToString($arCurrentActivity["Properties"]["Employees"], $arWorkflowTemplate, $documentType);
+                $arCurrentValues["AutoReplyContent"] = $arCurrentActivity["Properties"]["AutoReplyContent"];
+                $arCurrentValues["SetReadStatus"] = $arCurrentActivity["Properties"]["SetReadStatus"];
+            }
+        }
+
+        return $runtime->ExecuteResourceFile(
+            __FILE__,
+            "properties_dialog.php",
+            array(
+                "arCurrentValues" => $arCurrentValues,
+                "formName" => $formName,
+            )
+        );
     }
 
     /**
@@ -108,6 +142,23 @@ class CBPJCSetEmailAutoReplyActivity extends CBPActivity
      */
     public static function GetPropertiesDialogValues($documentType, $activityName, &$arWorkflowTemplate, &$arWorkflowParameters, &$arWorkflowVariables, $arCurrentValues, &$arErrors)
     {
+        $arErrors = array();
+
+        $arProperties = array(
+            "Employees" => CBPHelper::UsersArrayToString($arCurrentValues["Employees"], $arWorkflowTemplate, $documentType),
+            "AutoReplyContent" => $arCurrentValues["AutoReplyContent"],
+            "SetReadStatus" => $arCurrentValues["SetReadStatus"],
+        );
+
+        $arErrors = self::ValidateProperties($arProperties, new CBPWorkflowTemplateUser(CBPWorkflowTemplateUser::CurrentUser));
+        if (count($arErrors) > 0) {
+            return false;
+        }
+
+        $currentActivity = &CBPWorkflowTemplateLoader::FindActivityByName($arWorkflowTemplate, $activityName);
+        $currentActivity["Properties"] = $arProperties;
+
+        return true;
     }
 
     /**
@@ -120,6 +171,31 @@ class CBPJCSetEmailAutoReplyActivity extends CBPActivity
 
     public static function ValidateProperties($arTestProperties = array(), CBPWorkflowTemplateUser $user = null)
     {
+        $arErrors = array();
+
+        if (empty($arTestProperties["Employees"])) {
+            $arErrors[] = array(
+                "code" => "emptyText",
+                "parameter" => "Employees",
+                "message" => GetMessage("JC_WL2F_CONTENT_EMPTY"),
+            );
+        }
+        if (empty($arTestProperties["AutoReplyContent"])) {
+            $arErrors[] = array(
+                "code" => "emptyText",
+                "parameter" => "AutoReplyContent",
+                "message" => GetMessage("JC_WL2F_FILE_PATH_EMPTY"),
+            );
+        }
+        if (empty($arTestProperties["SetReadStatus"])) {
+            $arErrors[] = array(
+                "code" => "emptyText",
+                "parameter" => "SetReadStatus",
+                "message" => GetMessage("JC_WL2F_FILE_PATH_EMPTY"),
+            );
+        }
+
+        return array_merge($arErrors, parent::ValidateProperties($arTestProperties, $user));
     }
 
     /**
